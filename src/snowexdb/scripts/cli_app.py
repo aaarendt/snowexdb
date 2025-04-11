8ea8eca85a0deb7711211f6f5818ec2afc0ca76d
aaarendt <arendta@uw.edu>
2025-04-11 08:03:08 -0700

    initial commit

---
+++ src/snowexdb/scripts/cli_app.py
@@ -0,0 +1,10 @@
+import typer
+from snowexdb.scripts.popluate_database import db_populate_app
+
+# The parent app that will contain all child apps for different script types
+app = typer.Typer()
+
+app.add_typer(db_populate_app, name="populate_database")
+
+if __name__ == "__main__":
+    app()
