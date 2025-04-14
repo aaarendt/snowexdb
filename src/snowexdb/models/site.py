b884b5489bba3bce06e02d70912f1b24038d336f
aaarendt <arendta@uw.edu>
2025-04-14 12:48:41 -0700

    update documentation

--- src/snowexdb/models/site.py
+++ src/snowexdb/models/site.py
@@ -1,5 +1,4 @@
 import uuid
-from snowexdb.models.base import Base
 from snowexdb.models.single_location import SingleLocationData
 from typing import Optional, TYPE_CHECKING
 from sqlmodel import Field, Relationship
@@ -9,9 +8,53 @@ if TYPE_CHECKING:
 
 class Site(SingleLocationData, table=True):
     """
-    Table stores Site data. Does not store data values,
+    Table stores Site data. Does not store observed data values,
     it only stores the site metadata.
+
+    Attributes
+    ----------
+    id : uuid.UUID
+        The unique identifier for the user role, 
+        generated automatically using UUID.
+    name : str
+        The name of the site.
+    description : str
+        A brief description of the site.
+    slope_angle : float 
+        The site slope angle in degrees.
+    aspect : float
+        The site aspect in degrees.
+    air_temp : float
+        The air temperature at the site (°C).
+    total_depth : float
+        The total depth of the snowpack (m).
+    weather_description : str
+        A description of the weather conditions.
+    precip : str
+        The precipitation type or amount.
+    sky_cover : str
+        The sky cover conditions.
+    wind : str
+        The wind conditions at the site.
+    ground_condition : str
+        The condition of the ground at the site.
+    ground_roughness : str
+        The roughness of the ground surface.
+    ground_vegetation : str
+        The type of vegetation on the ground.
+    vegetation_height : str
+        The height of the vegetation (m).
+    tree_canopy : str
+        The tree canopy cover percentage.
+    site_notes : str
+        Additional notes about the site.
+    layers : Optional[Layer]
+        A relationship to the `Layer` model, with back_populates set to 
+        "site", linking a layer measurement to the site at which it was 
+        measured.
+        This is NOT a column in the table but represents relationship only.     
     """
+
     __tablename__ = 'sites'
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
