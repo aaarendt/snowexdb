55e1de9d9da86074f2f04144a3eec9d37dca8d3e
aaarendt <arendta@uw.edu>
2025-05-04 17:59:37 -0700

    modify for reading df and metadata obj from inistupy

--- src/snowexdb/scripts/populate_database.py
+++ src/snowexdb/scripts/populate_database.py
@@ -6,11 +6,12 @@ from snowexdb.models.instrument import Instrument
 from snowexdb.models.layer import Layer
 from snowexdb.models.site import Site
 from snowexdb.utils.projection import create_geom
-from datetime import datetime
 from pathlib import Path
 
 import logging
 
+from snowexdb.utils.data_parser import download_csv, parse_csv_data
+from snowexdb.utils.access_data import NSIDC_access
 
 INPUT_DIRECTORY = Path(__file__).parent / 'resources/data'
 
@@ -18,27 +19,30 @@ logger = logging.getLogger(__name__)
 
 db_populate_app = typer.Typer()
 
-
-def add_layer_data():
+def add_layer_data(profile_df, metadata):
     """
     Adds snow layer profile data to the database
+
+    Args:
+        profile_df (dataframe): the dataframe of observations
+        metadata (object): the metadata associated with the profile data
+    
     """
     instrument = add_instrument_data()
-    site = add_site_data()
+    site = add_site_data(site_metadata={"latitude": metadata.latitude,
+                                        "longitude": metadata.longitude,
+                                        "elevation": 400, # temporary
+                                        "name":metadata.site_name,
+                                        "date":metadata.date_time})
 
-    sample_layers = INPUT_DIRECTORY / 'layers.csv'
-    
-    with sample_layers.open(mode='r', newline='') as file:
-        csv_reader = csv.DictReader(file)
-
-        for row in csv_reader:
-            csv_data = Layer(depth = row["depth"],
-                              bottom_depth = row["bottom_depth"],
-                              value = row["value"],
-                              comments = row["comments"], 
-                              instrument_id = instrument.id,
-                              site_id = site.id)
-            BaseRepository.add(csv_data)
+    for index, row in profile_df.iterrows():
+        data = Layer(depth=row["depth"],
+                          bottom_depth=row["bottom_depth"],
+                          value=str(row["density"]), #temporary
+                          comments=metadata.comments, 
+                          instrument_id=instrument.id,
+                          site_id=site.id)
+        BaseRepository.add(data)
     logger.info("Layer Added Successfully")
 
 def add_instrument_data():
@@ -57,30 +61,41 @@ def add_instrument_data():
     logger.info("Instrument Added Successfully")
     return instrument
 
-def add_site_data():
+def add_site_data(site_metadata):
     """
     Adds site data to the database
-    """
-    metadata_file = INPUT_DIRECTORY / 'metadata.csv'
 
-    with metadata_file.open(mode='r', newline='') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            coordinates = create_geom({"epsg":4326,
-                                      "longitude":row['longitude'],
-                                      "latitude":row['latitude']})
-            site = Site(name=row['site_name'], 
-                        elevation=400,
-                        date = datetime.strptime(row['date'], '%m-%d-%Y'),
-                        geom=coordinates['geom'])
-        BaseRepository.add(site)    
+    Args:
+        site_metadata (dict): dictionary of site metadata elements
+    """
+
+    coordinates = create_geom({"epsg":4326,
+                                "longitude":site_metadata['longitude'],
+                                "latitude":site_metadata['latitude']})
+    site = Site(name=site_metadata['name'], 
+                elevation=site_metadata['elevation'],
+                date=site_metadata['date'],
+                geom=coordinates['geom'])
+    BaseRepository.add(site)    
     logger.info("Site Added Successfully")
     return site
 
-@db_populate_app.command(help="Command to add layer data to the database")
-def add_layer():
-    add_layer_data()
+@db_populate_app.command(help="Add density data to the database")
+def add_density():
+    collections = NSIDC_access("10.5067/KZ43HVLZV6G4")
+    for collection in collections:
+        files = collection.data_links("Data")
+        for file in files:
+            if "density" in file:
+                download_csv(file)
+                profileData = parse_csv_data(INPUT_DIRECTORY / 
+                                             "downloaded_file.csv")
+                for profile in profileData.profiles:
+                    add_layer_data(profile.df, profile.metadata)
+                print("{} file imported!".format(file))
 
-@db_populate_app.command(help="Add site data to the database")
-def add_site():
-    add_site_data()
+# TODO: determine right level of loops to add the site and instrument data, 
+# not for each profile object as being done now
+
+# for temporarily testing with a single file
+        #files = ["https://n5eil01u.ecs.nsidc.org/DP6/SNOWEX/SNEX20_TS_SP.002/2019.12.16/SNEX20_TS_SP_20191216_1232_COFEJ2_data_density_v02.csv"]
