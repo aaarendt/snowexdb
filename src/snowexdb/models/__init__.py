477b0dcf33211845406b23f1ce33a0dcea7c1888
aaarendt <arendta@uw.edu>
2025-05-06 13:38:45 -0700

    updated list to include the new models

--- src/snowexdb/models/__init__.py
+++ src/snowexdb/models/__init__.py
@@ -2,6 +2,12 @@ from snowexdb.models.layer import Layer
 from snowexdb.models.instrument import Instrument
 from snowexdb.models.site import Site
 from snowexdb.models.single_location import SingleLocationData
+from snowexdb.models.doi import DOI
+from snowexdb.models.campaign_observation import CampaignObservation
+from snowexdb.models.campaign import Campaign
+from snowexdb.models.measurement_type import MeasurementType
+from snowexdb.models.observers import Observer
+from snowexdb.models.site_observers import SiteObserversLink
 
 # New models created should be exposed by adding to __all__. This is used by SQLModel.metadata
 # https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata-order-matters
@@ -10,4 +16,10 @@ __all__ = [
     "Instrument",
     "Site",
     "SingleLocationData",
+    "DOI",
+    "CampaignObservation",
+    "Campaign",
+    "MeasurementType",
+    "Observer",
+    "SiteObserversLink",
 ]
